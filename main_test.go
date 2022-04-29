1326ab3624095e6b3667a22c19cead3fd36efc57
Xines <denflottejonas@hotmail.com>
2022-04-29 03:49:22 +0200

    Remove wrong placed code comment

--- main_test.go
+++ main_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 )
 
-// Brute forces & returns all possible combinations of standard Caesar cipher
 func TestBruteForceCaesarCipher(t *testing.T) {
 
 	for i, v := range BruteForceCaesarCipher("Xvezlj nzkyflk vultrkzfe zj czbv jzcmvi ze kyv dzev") {
